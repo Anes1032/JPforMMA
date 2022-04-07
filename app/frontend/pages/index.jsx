02a69cc725cbfb889bd6f668cbe83473c880c03a
Sena Moriwaki <moriwaki0302@gmail.com>
2022-04-07 14:56:26 +0800

    prettier設定追加

--- app/frontend/pages/index.jsx
+++ app/frontend/pages/index.jsx
@@ -1,16 +1,14 @@
-
 const Index = (data) => {
-  console.log(data.data)
+  console.log(data.data);
   return <div>Hello World!!</div>;
-}
-
+};
 
 export const getServerSideProps = async () => {
-  const requestUrl = `${process.env.NEXT_PUBLIC_API_URL}/posts` 
+  const requestUrl = `${process.env.NEXT_PUBLIC_API_URL}/posts`;
   const res = await fetch(requestUrl);
   const data = await res.json();
 
   return { props: { data } };
-}
+};
 
 export default Index;
